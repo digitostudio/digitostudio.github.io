6632b984ce1cadc8b579837abe61c6b888083726
cjeria <chjeria@gmail.com>
2016-06-29 20:32:46 -0700

    particle.js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//= require_tree .
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
